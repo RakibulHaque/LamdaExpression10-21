8bc5ae233048d5e4b86a454bf724baea76b2abbf
RakibulHaque <hrakibul@sun21.co.jp>
2019-10-25 17:40:28 +0900

    commit

---
+++ src/Greeter.java
@@ -0,0 +1,16 @@
+
+public class Greeter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyLamda myLambdaFunction = () -> System.out.println("Hello world");
+		
+
+	}
+
+}
+
+interface MyLamda
+{
+	void foo();
+}
---
+++ src/LamdaExample.java
@@ -0,0 +1,15 @@
+
+1.GreetingFunction = () -> Ssystem.out.println("Hello World");
+2.DoubleNumberFunction = (int a) -> a*2;
+3.addFunction = (int a,int b) -> a+b;
+4.saveDivideFunction = (int a,int b) -> 
+		{
+			if(b==0)
+			{
+				return 0;
+			}
+			else
+				return a/b;
+		};
+5.(String s) -> s.length();
+
